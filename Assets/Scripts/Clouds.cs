f37de69eb674a5bd029ff4a2f984ec3d67d7ab82
Idemax <idemax@gmail.com>
2019-01-17 17:47:00 -0400

    each cloud take care of your movement

--- Assets/Scripts/Clouds.cs
+++ Assets/Scripts/Clouds.cs
@@ -4,40 +4,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Clouds : MonoBehaviour {
-    public List<GameObject> CloudsMap;
-    float delay = 0.0f;
-    float speedRate = 0.0f;
+    // public List<GameObject> CloudsMap;
+    // float delay = 0.0f;
+    // float speedRate = 0.0f;
 
-    void Start () {
-        foreach (Transform cloud in transform) {
-            if (cloud.tag == "Cloud") {
-                CloudsMap.Add (cloud.gameObject);
-                cloud.gameObject.transform.position = new Vector3 (17.0f, cloud.gameObject.transform.position.y, cloud.gameObject.transform.position.z);
-                // cloud.gameObject.transform.GetComponent<SpriteRenderer> ().orderInLayer = Random.Range (-1, 1);
-            }
-        }
-    }
+    // void Start () {
+    //     foreach (Transform cloud in transform) {
+    //         if (cloud.tag == "Cloud") {
+    //             CloudsMap.Add (cloud.gameObject);
+    //             cloud.gameObject.transform.position = new Vector3 (17.0f, cloud.gameObject.transform.position.y, cloud.gameObject.transform.position.z);
+    //             // cloud.gameObject.transform.GetComponent<SpriteRenderer> ().orderInLayer = Random.Range (-1, 1);
+    //         }
+    //     }
+    // }
 
-    void Update () {
-        int delayIndex = 0;
+    // void Update () {
+    //     int delayIndex = 0;
 
-        delay += Time.deltaTime;
+    //     delay += Time.deltaTime;
 
-        foreach (GameObject cloud in CloudsMap) {
-            if (cloud.transform.position.x <= 16.0f && cloud.transform.position.x >= -25.0f) {
-                if (delay > 2.0f * delayIndex++) {
-                    cloud.transform.Translate (Vector3.left * Time.deltaTime * (5.0f + speedRate));
-                }
-            }
-            else {
-                float yPos = Random.Range (-6.0f, 6.0f);
+    //     foreach (GameObject cloud in CloudsMap) {
+    //         if (cloud.transform.position.x <= 16.0f && cloud.transform.position.x >= -25.0f) {
+    //             if (delay > 2.0f * delayIndex++) {
+    //                 cloud.transform.Translate (Vector3.left * Time.deltaTime * (5.0f + speedRate));
+    //             }
+    //         }
+    //         else {
+    //             float yPos = Random.Range (-6.0f, 6.0f);
 
-                cloud.gameObject.transform.position = new Vector3 (16.0f, yPos, cloud.gameObject.transform.position.z);
-                // cloud.gameObject.transform.position = new Vector3 (16.0f, cloud.gameObject.transform.position.y, cloud.gameObject.transform.position.z);
+    //             cloud.gameObject.transform.position = new Vector3 (16.0f, yPos, cloud.gameObject.transform.position.z);
+    //             // cloud.gameObject.transform.position = new Vector3 (16.0f, cloud.gameObject.transform.position.y, cloud.gameObject.transform.position.z);
 
-                delayIndex++;
-                speedRate += 0.01f;
-            }
-        }
-    }
+    //             delayIndex++;
+    //             speedRate += 0.01f;
+    //         }
+    //     }
+    // }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public static int index = 0;
+
+    int instanceIndex;
+    float speedRate = 0.0f;
+    float speedRateBase;
+    float objectScale;
+    float delay = 0.0f;
+
+    void Start () {
+        instanceIndex = index++;
+        // speedRateBase = Random.Range (3.0f, 6.0f);
+
+        Debug.Log (instanceIndex);
+
+        transform.position = new Vector3 (
+            18.0f,
+            transform.position.y,
+            transform.position.z
+        );
+    }
+
+    void Update () {
+        delay += Time.deltaTime;
+
+        if (transform.position.x <= 16.0f && transform.position.x >= -25.0f) {
+            if (delay > (2.0f * instanceIndex)) {
+                transform.Translate (Vector3.left * Time.deltaTime * (speedRateBase + speedRate));
+            }
+        }
+        else {
+            float yPos = Random.Range (-6.0f, 6.0f);
+
+            speedRate += 0.01f;
+            speedRateBase = Random.Range (3.0f, 6.0f);
+            objectScale = Random.Range (0.2f, 0.6f);
+
+            transform.position = new Vector3 (16.0f, yPos, transform.position.z);
+            transform.localScale = new Vector3 (objectScale, objectScale, transform.localScale.z);
+        }
+    }
+}
