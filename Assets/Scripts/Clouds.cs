b488aba4739c5025e91c5f4c4c05642f406d1cbe
Idemax <idemax@gmail.com>
2019-01-17 14:52:26 -0400

    first commit

---
+++ Assets/Scripts/Clouds.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+    public List<GameObject> CloudsMap;
+    float delay = 0.0f;
+    float speedRate = 0.0f;
+
+    void Start () {
+        foreach (Transform cloud in transform) {
+            if (cloud.tag == "Cloud") {
+                CloudsMap.Add (cloud.gameObject);
+                cloud.gameObject.transform.position = new Vector3 (17.0f, cloud.gameObject.transform.position.y, cloud.gameObject.transform.position.z);
+            }
+        }
+    }
+
+    void Update () {
+        int delayIndex = 1;
+
+        delay += Time.deltaTime;
+
+        foreach (GameObject cloud in CloudsMap) {
+            if (cloud.transform.position.x <= 16.0f && cloud.transform.position.x >= -17.0f) {
+                if (delay > 3.0f * delayIndex++) {
+                    cloud.transform.Translate (Vector3.left * Time.deltaTime * (6.0f + speedRate));
+                }
+            }
+            else {
+                float yPos = Random.Range (-5.0f, 5.0f);
+
+                cloud.gameObject.transform.position = new Vector3 (16.0f, yPos, cloud.gameObject.transform.position.z);
+
+                delayIndex++;
+                speedRate += 0.01f;
+            }
+        }
+    }
+}
